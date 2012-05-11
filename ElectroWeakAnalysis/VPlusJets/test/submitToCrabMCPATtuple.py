b2f12dfe4fc7a5ff12ee67fda6270bd7058c1f44
ntran <>
2012-05-11 18:18:45 +0000

    updates to submit the latest set of pattuples

--- ElectroWeakAnalysis/VPlusJets/test/submitToCrabMCPATtuple.py
+++ ElectroWeakAnalysis/VPlusJets/test/submitToCrabMCPATtuple.py
@@ -2,13 +2,15 @@ import os,sys
 import string, re
 from time import gmtime, localtime, strftime
 
+outputLocationOnEos = "ntran/tlbsm_v10_FallV3"
+
 dataset    = [ 
 ##               # diboson
-##               "/WW_TuneZ2_7TeV_pythia6_tauola/kalanand-ttbsm_v10beta_WWtoAnything_Fall11-PU_S6_START42_V14B-a326ba49a16ab761c492392538b61378/USER",
-##               "/WZ_TuneZ2_7TeV_pythia6_tauola/kalanand-ttbsm_v10beta_WZtoAnything_Fall11-PU_S6_START42_V14B-a326ba49a16ab761c492392538b61378/USER",
-##               "/ZZ_TuneZ2_7TeV_pythia6_tauola/kalanand-ttbsm_v10beta_ZZtoAnything_Fall11-PU_S6_START42_V14B-a326ba49a16ab761c492392538b61378/USER",
+              "/WW_TuneZ2_7TeV_pythia6_tauola/kalanand-ttbsm_v10beta_WWtoAnything_Fall11-PU_S6_START42_V14B-a326ba49a16ab761c492392538b61378/USER",
+              "/WZ_TuneZ2_7TeV_pythia6_tauola/kalanand-ttbsm_v10beta_WZtoAnything_Fall11-PU_S6_START42_V14B-a326ba49a16ab761c492392538b61378/USER",
+              "/ZZ_TuneZ2_7TeV_pythia6_tauola/kalanand-ttbsm_v10beta_ZZtoAnything_Fall11-PU_S6_START42_V14B-a326ba49a16ab761c492392538b61378/USER",
 ##               # ttbar
-##               "/TTJets_TuneZ2_7TeV-madgraph-tauola/kalanand-ttbsm_v10beta_TTJets_Fall11-PU_S6_START42_V14B-a326ba49a16ab761c492392538b61378/USER",
+              "/TTJets_TuneZ2_7TeV-madgraph-tauola/kalanand-ttbsm_v10beta_TTJets_Fall11-PU_S6_START42_V14B-a326ba49a16ab761c492392538b61378/USER",
 ##               # W+jets
 ##               "/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/ntran-ttbsm_v10_WJetsToLNu_TuneZ2_7TeV-madgraph-tauola_Fall11-PU_S6_START42_V14B-v1-a326ba49a16ab761c492392538b61378/USER",
               "/WJetsToLNu_PtW-100_TuneZ2_7TeV-madgraph/dlopes-ttbsm_v10beta_WJetsToLNu_PtW-100_TuneZ2_7TeV-madgraph-a326ba49a16ab761c492392538b61378/USER",
@@ -30,11 +32,11 @@ dataset    = [
 
 channels   = [
 ## ##     "WJets",
-##     "ch_WWtoAnything",    
-##     "ch_WZtoAnything",    
-##     "ch_ZZtoAnything",
+    "ch_WWtoAnything",    
+    "ch_WZtoAnything",    
+    "ch_ZZtoAnything",
     
-##     "ch_TTbar",
+    "ch_TTbar",
     
 ##     "ch_WJets_inclusive",
     "ch_WJets_boostedMadGraph",
@@ -55,7 +57,7 @@ channels   = [
 ]
 
 condor   = [
-##             1,1,1,1,1,
+            1,1,1,1,1,
             1,1,1,1,1,
             1,1,1,1,1,
             1,1
@@ -72,7 +74,9 @@ def changeCrabTemplateFile(index):
         if  line.find("mypublishdataname")!=-1:
             line=line.replace("mypublishdataname", "ttbsm_v10beta_"+channels[index]+"_Fall11-PU_S6_START42_V14B")
         if  line.find("mypdirname")!=-1:
-            line=line.replace("mypdirname",channels[index])                        
+            line=line.replace("mypdirname",channels[index]) 
+        if  line.find("LOCATION")!=-1:
+            line=line.replace("LOCATION", outputLocationOnEos)        
         if line.find("glite")!=-1 and condor[index]!=0:
             line=line.replace("glite", "condor")        
         fout.write(line)        
@@ -100,7 +104,7 @@ def setupFJ3(index):
 ###################
 for i in range(len(channels)):
     changeCrabTemplateFile(i)    
-    submitcommand = "crab -create -cfg " + "crabjob_" + channels[i] + ".cfg"
-    child   = os.system(submitcommand)
-    setupFJ3(i)
-    child2   = os.system("crab -submit -c "+channels[i])
+#    submitcommand = "crab -create -cfg " + "crabjob_" + channels[i] + ".cfg"
+#    child   = os.system(submitcommand)
+#    setupFJ3(i)
+#    child2   = os.system("crab -submit -c "+channels[i])
