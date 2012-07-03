e19791ee772a505fd1b2829f412660b5af2e844d
yygao <>
2012-07-03 14:31:25 +0000

    update for 20 jobs

--- JHUGen/analysis/runMany.sh
+++ JHUGen/analysis/runMany.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# run 20 jobs simultaneously
 
-# run the code...
-
-for SEED in 9472010 2316565 4849736 9574769 7443053 5400436 7399529 7599437 6586366 3156376; do
+for SEED in 9472010 2316565 4849736 9997417 7443053 5400436 7399529 7599437 6586366 3156376 9574769 8044030 3156379 5196721 1685724 4755297 3923139 2216676 2131904 3033528; do
+    #echo $SEED
+    #echo "nohup root -l  -q runSigSepWW.C\(${SEED}\) >& temp_${SEED}.log & "
     nice nohup root -l  -q runSigSepWW.C\(${SEED}\) >& temp_${SEED}.log &
 done
 
+# echo $RANDOM
