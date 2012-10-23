74b0ead80cb262312df7269c987a34de4f2bd451
zixu <>
2012-10-23 06:37:51 +0000

    script for CombinedLimit

---
+++ boostedWW/boostedWWAnalysis/combine.sh
@@ -0,0 +1,2 @@
+combine -M Asymptotic --minimizerAlgo Minuit2 --minosAlgo stepping -n HWWlvjj -m 600 -d hwwlvj_datacard_counting.txt
+combine -M Asymptotic --minimizerAlgo Minuit2 --minosAlgo stepping -n HWWlvjj -m 600 -d hwwlvj_datacard_unbin.txt --unbinned
