9af73275203b39fc12341061a5235774a9c7ceb7
zixu <>
2012-12-26 04:51:02 +0000

    *** empty log message ***

--- boostedWW/boostedWWAnalysis/combine.sh
+++
@@ -1,2 +0,0 @@
-combine -M Asymptotic --minimizerAlgo Minuit2 --minosAlgo stepping -n HWWlvjj -m 600 -d hwwlvj_datacard_counting.txt
-combine -M Asymptotic --minimizerAlgo Minuit2 --minosAlgo stepping -n HWWlvjj -m 600 -d hwwlvj_datacard_unbin.txt --unbinned
