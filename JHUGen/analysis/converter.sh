8b4531a91ee9ed49cf8a0b3e024a80eb084d5d84
yygao <>
2013-03-15 14:12:12 +0000

    from /scratch0/hep/whitbeck/4lHelicity/JHUgenerator/V02-02-01/JHUGen/converter.sh

--- JHUGen/analysis/converter.sh
+++ JHUGen/analysis/converter.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
 f=$1
+
+if [ $# == 1 ] 
+    then 
+    applySmear="false"
+    elif [ $# == 2 ] 
+    then
+    applySmear=$2
+    fi
+
 fr="${f%.*}"
-ff="${f%.*}.txt"
+ff="${f%.*}_${applySmear}.txt"
 echo $f, $ff
 
 awk '/<event>/,/LesHouchesEvents>/' $f | grep -iv "<event>" | grep -iv "</event>" | grep -iv "</LesHouchesEvents>" | grep -iv "9 100" | grep -iv "1 -1  0"  | grep -iv "2 -1  0" | grep -iv "3 -1  0" | grep -iv "4 -1  0" | grep -iv "5 -1  0"  | grep -iv "21 -1  0" | grep -iv "23  2  3" | grep -iv "39  2  1" | grep -iv "# " | grep -iv "<!-- " > $ff
 
+
 frt='"'$fr'"'
 echo $frt
-root -l -q -b "readOutAngles_LMH.C(${frt})"
-rm $ff
+
+if [ $applySmear == "true" ]
+    then
+    root -l -q -b "readOutAngles_LMH.C(${frt},true)"
+    elif [ $applySmear == "false" ] 
+    then
+    root -l -q -b "readOutAngles_LMH.C(${frt},false)"
+    else
+    echo "I don't recognize your second option..." 
+    fi
+
+#rm $ff
 
 echo "done"
 
